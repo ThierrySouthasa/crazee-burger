0a010a57c65b9914121256f832b65865c4048024
Thierry <southasa.thierry@gmail.com>
2023-03-05 00:28:31 +0100

    💄 F05 : ORDER > Product > Add img layout

--- src/components/pages/order/Main/Menu.js
+++ src/components/pages/order/Main/Menu.js
@@ -29,21 +29,6 @@ const MenuStyled = styled.div`
     justify-items: center;
     box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
 
-    .produit {
-        background-color: red;
-        width: 240px;
-        height: 330px;
-
-        .image {
-            width: 100px;
-            height: auto;
-
-            img {
-                width: 100%;
-                height: 100%;
-            }
-        }
-    }
 `
 
 export default Menu;
--- src/components/pages/order/Main/Product.js
+++ src/components/pages/order/Main/Product.js
@@ -1,20 +1,47 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const Product = ({ imageSource, title, price }) => {
     return (
-        <div className="produit">
+        <ProductStyled className="produit">
             <div className="image">
                 <img src={imageSource} alt={title} />
             </div>
             <div className="info-text">
+                <div className="title">{title}</div>
                 <div className="description">
-                    <div className="title">{title}</div>
                     <div className="price">{price}</div>
                     <button className='add-button'>Ajouter</button>
                 </div>
             </div>
-        </div>
+        </ProductStyled>
     );
 };
+
+const ProductStyled = styled.div`
+    background-color: red;
+    width: 200px;
+    height: 300px;
+    display: grid;
+    grid-template-rows: 65% 1fr;
+    padding: 20px;
+    padding-bottom: 10px;
+
+        .image {
+            width: 100%;
+            height: auto;
+            margin-top: 30px;
+
+            img {
+                width: 100%;
+                height: 100%;
+                object-fit: contain;
+            }
+        }
+
+        .description {
+
+        }
+`;
 
 export default Product;
