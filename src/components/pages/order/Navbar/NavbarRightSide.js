19b107219ebf3c2807c238b355adf438a9712e48
Thierry <southasa.thierry@gmail.com>
2023-03-12 15:11:56 +0100

    ✨ F06 : NAVBAR > Add notification toast on button admin click

--- src/components/pages/order/Navbar/NavbarRightSide.js
+++ src/components/pages/order/Navbar/NavbarRightSide.js
@@ -1,13 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
 import styled from 'styled-components/macro';
 import ToggleButton from '../../../../assets/ToggleButton';
 import Profile from './Profile';
+import 'react-toastify/dist/ReactToastify.css';
+import { theme } from "../../../../assets/theme"
+import { FaUserSecret } from "react-icons/fa"
 
 const NavbarRightSide = ({ firstName }) => {
+
+    const [isModeAdmin, setIsModeAdmin] = useState(false)
+
+
+    const displatToastNotification = () => {
+        if (isModeAdmin) {
+            toast.info("Mode admin activé", {
+                icon: <FaUserSecret size={30} />,
+                theme: "dark",
+                position: "bottom-right",
+                autoClose: 4500,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+            })
+        }
+        setIsModeAdmin(!isModeAdmin)
+    }
+
     return (
         <NavbarRightSideStyled>
-            <ToggleButton labelIfChecked='DÉSACTIVER LE MODE ADMIN' labelIfUnchecked='ACTIVER LE MODE ADMIN' />
+            <ToggleButton labelIfChecked='DÉSACTIVER LE MODE ADMIN' labelIfUnchecked='ACTIVER LE MODE ADMIN' onToggle={displatToastNotification} />
             <Profile firstName={firstName} />
+            <ToastContainer className="toaster" bodyClassName="body-toast" />
         </NavbarRightSideStyled>
     );
 };
@@ -18,6 +44,24 @@ const NavbarRightSideStyled = styled.div`
     align-items: center;
     padding-right: 50px;
     font-family: "Open Sans", sans-serif;
+
+    .toaster {
+    max-width: 300px;
+  }
+
+  .Toastify__toast.Toastify__toast-theme--dark.Toastify__toast--info {
+    background: ${theme.colors.background_dark};
+  }
+
+  .body-toast {
+    .Toastify__toast-icon.Toastify--animate-icon.Toastify__zoom-enter {
+      margin-right: 20px;
+      margin-left: 5px;
+    }
+    div {
+      line-height: 1.3em;
+    }
+  }
 `
 
 export default NavbarRightSide;
