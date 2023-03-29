779b244360766d71bd6e9f583d8e131e5fd4c2b5
Thierry <southasa.thierry@gmail.com>
2023-03-29 22:06:30 +0200

    💄 F07 : MAIN > Add layout admin

--- src/components/pages/order/Main/Main.js
+++ src/components/pages/order/Main/Main.js
@@ -8,21 +8,39 @@ const Main = () => {
     return (
         <MainStyled>
             <Basket />
-            <Menu />
+            <div className="menu-and-admin">
+                <Menu />
+                <div className="admin">Admin</div>
+            </div>
         </MainStyled>
     );
 };
 
 const MainStyled = styled.div`
     background-color: ${theme.colors.background_white};
-    flex: 1;  
+    height: calc(95vh - 10vh);
     border-bottom-left-radius: ${theme.borderRadius.extraRound};
     border-bottom-right-radius: ${theme.borderRadius.extraRound};
     box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
     display: grid;
     grid-template-columns: 20% 1fr;
 
-    overflow-y: scroll;
+    .menu-and-admin {
+        position: relative;
+        overflow-y: hidden;
+        display: grid;
+        //border-bottom-left-radius: ${theme.borderRadius.extraRound};
+        border-bottom-right-radius: ${theme.borderRadius.extraRound};
+
+        .admin {
+            background-color: red;
+            position: absolute;
+            height: 250px;
+            bottom: 0;
+            left: 0;
+            right: 0;
+        }
+    }
 
 `
 
