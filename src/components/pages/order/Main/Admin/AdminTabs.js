dd2f8a0a3e779f66f737803fb3a13230b9862402
Thierry <southasa.thierry@gmail.com>
2023-04-07 12:38:24 +0200

    💄 F07: ADMIN > Tabs > Add Tab

--- src/components/pages/order/Main/Admin/AdminTabs.js
+++ src/components/pages/order/Main/Admin/AdminTabs.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import styled from 'styled-components';
+import Tab from '../../../../../assets/Tab';
+import { FiChevronDown, FiChevronUp } from "react-icons/fi"
 
 const AdminTabs = () => {
     return (
         <AdminTabsStyled>
-
+            <Tab Icon={<FiChevronDown />} />
         </AdminTabsStyled>
     );
 };
 
 const AdminTabsStyled = styled.div`
-  
+  display: flex;
+  padding: 0 20px;
 `;
 
 export default AdminTabs;
